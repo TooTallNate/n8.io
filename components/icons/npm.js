61de34c1fd96dfeefdcbdca0574e8be071ece64b
Nathan Rajlich <nathan@tootallnate.net>
2019-06-05 12:19:11 -0700

    Fix warning

--- components/icons/npm.js
+++ components/icons/npm.js
@@ -4,7 +4,7 @@ export default props => (
     xmlns="http://www.w3.org/2000/svg"
     { ...props }
   >
-    <path class="colored outer" d="M0,16V0H16V16ZM3,3V13H8V5h3v8h2V3Z"/>
-    <path class="colored inner" d="M3,3H13V13H11V5H8v8H3Z"/>
+    <path className="colored outer" d="M0,16V0H16V16ZM3,3V13H8V5h3v8h2V3Z"/>
+    <path className="colored inner" d="M3,3H13V13H11V5H8v8H3Z"/>
   </svg>
 );
