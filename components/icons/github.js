545c6880584a69c01f8cdf6d11756c0bff1ab2a4
Nathan Rajlich <nathan@tootallnate.net>
2019-06-05 12:16:54 -0700

    Update to Now v2

--- components/icons/github.js
+++ components/icons/github.js
@@ -1,4 +1,4 @@
-module.exports = props => (
+export default props => (
   <svg
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
--- components/icons/npm.js
+++ components/icons/npm.js
@@ -1,4 +1,4 @@
-module.exports = props => (
+export default props => (
   <svg
     viewBox="0 0 16 16"
     xmlns="http://www.w3.org/2000/svg"
