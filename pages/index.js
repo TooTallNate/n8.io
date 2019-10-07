f77864483e6d79682de165f87edbec7d28750f37
Nathan Rajlich <nathan@tootallnate.net>
2019-10-07 14:03:25 -0700

    Update to Next.js 9.1.0
    See: https://nextjs.org/blog/next-9-1

--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,9 @@ export default props => {
           content="initial-scale=1.0, width=device-width, user-scalable=no"
           key="viewport"
         />
-        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="64x64" href="/static/favicon-64x64.png" />
-        <link rel="icon" type="image/png" sizes="128x128" href="/static/favicon-128x128.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="64x64" href="/favicon-64x64.png" />
+        <link rel="icon" type="image/png" sizes="128x128" href="/favicon-128x128.png" />
         <link href="https://sf.n8.io/?weight=100" rel="stylesheet" />
       </Head>
 
